c4b5c4b353a4677202be310d7ca12982d61855b0
Sergio De Pietro <sergiodp@creacttive.com>
2016-10-12 19:00:19 -0300

    varios

--- application/views/admin/template/header.php
+++ application/views/admin/template/header.php
@@ -1,5 +1,5 @@
 <head>
-    <title>Matrix Admin</title>
+    <title>Woopi Blog - Admin Panel</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="<?= base_url() ?>assets/css/bootstrap.min.css" />
